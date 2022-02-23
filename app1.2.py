4de1b9ec059e3d8ec90c293bf63eecbef34b6a1f
Zakariya <91939621+mzakariyaf@users.noreply.github.com>
2022-02-23 13:45:25 +0530

    Update app1.2.py

--- app1.2.py
+++ app1.2.py
@@ -2,10 +2,10 @@ import mysql.connector
 from difflib import get_close_matches
 
 con = mysql.connector.connect(
-    user = "ardit700_student",
-    password = "ardit700_student",
-    host = "108.167.140.122",
-    database = "ardit700_pm1database"
+    user = "user_name",
+    password = "password",
+    host = "ip",
+    database = "db_name"
 )
 
 cursor = con.cursor()  
